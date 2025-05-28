88644b1b2dbc807bc5fe66d6434f0baade58c26b
Ghanshyam Mandal <ghanshyam930400@gmail.com>
2025-05-28 18:59:49 +0530

    feat(user): created shop listing page UI

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth-slice';
-import adminProudctsSlice from './admin/products-slice'
+import adminProudctsSlice from './admin/products-slice';
+import shopProductsSlice from './shop/product-slice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    adminProducts: adminProudctsSlice
+    adminProducts: adminProudctsSlice,
+    shopProducts: shopProductsSlice,
   },
 });
 
